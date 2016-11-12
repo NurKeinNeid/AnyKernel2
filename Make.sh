7dd4e45757d9334fd6b762cc682cf21b3e4e66de
nobe1976 <charlesmc76@gmail.com>
2016-11-12 15:19:33 -0500

    Add back Hammerhead to build script

--- Make.sh
+++ Make.sh
@@ -6,9 +6,29 @@ ENDCOLOR="\033[0m"
 
 cd && cd Unicornblood_N
 
-rm -rf AnyKernel2-SHAMU AnyKernel2-BULLHEAD AnyKernel2-ANGLER
+rm -rf AnyKernel2-HAMMERHEAD AnyKernel2-SHAMU AnyKernel2-BULLHEAD AnyKernel2-ANGLER
 
 repo sync
+
+echo -e $YELLOW"Lets build Hammerhead..."$ENDCOLOR
+
+export PATH=$HOME/Unicornblood_N/prebuilt/arm-eabi-4.8/bin:$PATH
+
+export ARCH=arm && export CROSS_COMPILE=arm-eabi-
+
+cd android_kernel_lge_hammerhead && make clean && make mrproper
+
+make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE hammerhead_defconfig
+
+make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE -j16
+
+cd && cd Unicornblood_N
+
+cp -v android_kernel_lge_hammerhead/arch/arm/boot/zImage-dtb AnyKernel2-HAMMERHEAD/zImage-dtb
+cd AnyKernel2-HAMMERHEAD
+zip -r9 Unicornblood_N-HAMMERHEAD-$(date +%m%d%Y).zip * -x README Unicornblood_N-HAMMERHEAD-$(date +%m%d%Y).zip
+
+cd && cd Unicornblood_N
 
 echo -e $YELLOW"Lets build Shamu..."$ENDCOLOR
 
